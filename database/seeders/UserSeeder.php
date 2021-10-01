7ce96d847218daff89cdcbb6ac41de6775250a5d
Lapat <lapat.s@ku.th>
2021-10-01 18:28:37 +0700

    add user hardcode seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User();
+        $user->name =  "AUI2488";
+        $user->email = "aui2488@aui.com";
+        $user->password = "Aui2488";
+        $user->role = "user";
+        $user->save();
+    }
+}
