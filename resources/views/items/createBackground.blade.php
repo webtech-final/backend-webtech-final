5b48bd0b11da36159337783368b0307add26e503
Panupat <panupat.ch@ku.th>
2021-10-16 03:31:00 +0700

    update Add New Background UI

--- resources/views/items/createBackground.blade.php
+++ resources/views/items/createBackground.blade.php
@@ -1,44 +1,45 @@
 @extends('layouts.main')
 @section('content')
-
-    <div class="text-6xl font-serif">
-        Add New Background
-    </div>
-    <div class="w-1/2 mx-auto bg-gray-300 py-6 px-8 mt-8">
-        <form action="{{ route('items.storeBackground') }} " method="post" enctype="multipart/form-data"
-            class=" ">
-            @csrf
-            <div class="space-y-2">
-                <label for="name" id="lbName" class="text-2xl">Name</label>
-                <input class="w-3/4 block rounded-lg " type="text" name="name" placeholder="name" placeholder="name">
-            </div>
-            @error('name')
-                <div>
-                    <span class="text-red-600">{{ $message }}</span>
+    <div class="mt-10 bg-gray-900 mx-64 p-24 rounded-xl">
+        <div class="text-6xl font-serif text-center text-white">
+            Add New Background
+        </div>
+        <div class="w-full mx-auto py-6 px-8 mt-10">
+            <form action="{{ route('items.storeBackground') }} " method="post" enctype="multipart/form-data"
+                class=" ">
+                @csrf
+                <div class="space-y-2">
+                    <label for="name" id="lbName" class="text-2xl font-mono text-white">Name</label>
+                    <input class="w-full text-xl block rounded-lg " type="text" name="name" placeholder="name" placeholder="name">
                 </div>
-            @enderror
-            <div class="space-y-2 mt-2">
-                <label for="Point" class="text-2xl">Price</label>
-                <input type="text" class="w-3/4 block rounded-lg " name="point" placeholder="point">
-            </div>
-            @error('price')
-                <div>
-                    <span class="text-red-600">{{ $message }}</span>
+                @error('name')
+                    <div>
+                        <span class="text-red-600">{{ $message }}</span>
+                    </div>
+                @enderror
+                <div class="space-y-2 mt-3 w-full">
+                    <label for="Point" class="text-2xl font-mono text-white">Price</label>
+                    <input type="text" class="w-full text-xl block rounded-lg " name="point" placeholder="point">
                 </div>
-            @enderror
-            <div class="space-y-2 mt-2">
-                <label for="image" class="text-2xl">Background Image</label>
-                <input type="file" name="backgroundImage" class="w-full">
-            </div>
-            @error('backgroundImage')
-                <div>
-                    <span class="text-red-600">{{ $message }}</span>
+                @error('price')
+                    <div>
+                        <span class="text-red-600">{{ $message }}</span>
+                    </div>
+                @enderror
+                <div class="space-y-2 mt-3">
+                    <label for="image" class="text-2xl font-mono text-white">Background Image</label>
+                    <input type="file" name="backgroundImage" class="w-full text-xl bg-white w-full">
                 </div>
-            @enderror
-            <div class="mt-4 text-right ">
-                <button type="submit"
-                    class="bg-green-400 font-semibold text-2xl border-2 px-6 py-2 rounded-full hover:bg-green-200 hover:shadow-xl">Add</button>
-            </div>
-        </form>
+                @error('backgroundImage')
+                    <div>
+                        <span class="text-red-600">{{ $message }}</span>
+                    </div>
+                @enderror
+                <div class="mt-10">
+                    <button type="submit"
+                        class="w-full bg-green-400 font-mono text-2xl px-6 py-2 rounded-full hover:bg-green-500 hover:shadow-xl">Add</button>
+                </div>
+            </form>
+        </div>
     </div>
 @endsection
