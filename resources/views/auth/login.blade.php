391825f2f649609605f99c82a879c7bda10ab55a
bigbigbig <krisada.m@ku.th>
2021-10-17 18:01:41 +0700

    added logo to login page and tab, edited nav bar

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
         <x-auth-card>
             <x-slot name="logo">
                 <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                    <img src=" logo.png " alt="" width="200" height="250">
                 </a>
             </x-slot>
 
@@ -44,13 +44,6 @@
                 </div>
 
                 <div class="flex items-center justify-end mt-4">
-                    @if (Route::has('password.request'))
-                        <a class="underline text-sm text-gray-600 hover:text-gray-900"
-                            href="{{ route('password.request') }}">
-                            {{ __('Forgot your password?') }}
-                        </a>
-                    @endif
-
                     <x-button class="ml-3">
                         {{ __('Log in') }}
                     </x-button>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Tetris.VS ADMIN</title>
+    <link rel="icon" type="image/png" sizes="16x16" href="/logo.png">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <script src="{{ asset('css/app.css') }}"></script>
 </head>
@@ -13,22 +14,23 @@
 <body class="bg-gray-100 space-y-5">
     <!-- nav -->
     <div class="py-4 px-10 bg-gray-800 space-x-2 flex justify-between items-center">
-        <div>
-            @if (Auth::check())
+        @if (Auth::check())
+            <div>
                 <a href="/items" class="py-2 px-5 text-white rounded-full hover:bg-gray-700 text-xl">Item
                     Management</a>
                 <a href="/rate" class="py-2 px-5 text-white rounded-full hover:bg-gray-700 text-xl">Point Rate</a>
+            </div>
+            <div>
                 <form action="{{ route('logout') }}" method="post" class="inline-block">
                     @csrf
                     <button class="justify-self-end py-2 px-5 text-white rounded-full hover:bg-gray-700 text-xl">Logout
                     </button>
                 </form>
-            @else
-                <a href="{{ route('login') }}"
-                    class="justify-self-end py-2 px-5 text-white rounded-full hover:bg-gray-700 text-xl">Login</a>
-            @endif
-
-        </div>
+            </div>
+        @else
+            <a href="{{ route('login') }}"
+                class="justify-self-end py-2 px-5 text-white rounded-full hover:bg-gray-700 text-xl">Login</a>
+        @endif
     </div>
     <div class="container mx-auto min-h-screen">
         @yield('content')
